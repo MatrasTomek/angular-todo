159995c41161e4cb56f1d19d48b57ded2c5ad287
Tomasz Matras <tomasz.matras@developerweb.pl>
2023-05-11 22:41:05 +0200

    Basic app and task interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  config: { [key: string]: string | Date } | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2023-01-01',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2023-03-01',
+      done: false,
+    },
+    {
+      name: 'Sprzatanie kuwety',
+      deadline: '2023-05-01',
+      done: false,
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: ' Lista zadań w Angular',
+        date: new Date(),
+      };
+    }, 1500);
+  }
 }
